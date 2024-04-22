d9fc3cdb17bb75e8adc7ec14c0c837a71a26f1f9
Thaliavch <71163693+Thaliavch@users.noreply.github.com>
2024-04-22 19:23:54 -0400

    Update Project Queries.sql
    The first 4 queries are the ones we had before, the rest are new

--- Project Queries.sql
+++ Project Queries.sql
@@ -1,10 +1,5 @@
---Question 1:
-create view rabbit_owner_count as
-	select count(*)
-	from customer
-	where customer_id in (select distinct customer_id
-						  from customer join pet on customer_id = owner_id
-						  where species = 'Rabbit');
+-- Question 1
+select * from stock
 						  
 --Question 2:
 select breed, count(*), dense_rank() over (order by count(*) desc) as rank
@@ -13,115 +8,7 @@ where species = 'Cat'
 group by breed
 order by rank;
 
---Question 3:
-update stock
-	set quantity = quantity + 30
-	where item_id between 21 and 30; --health products
-	
---Question 4:
-CREATE OR REPLACE FUNCTION most_valuable_users()
-	RETURNS TABLE(id NUMERIC(8,0), name VARCHAR(30),
-		points NUMERIC(5,0)) AS $$
-BEGIN
-	RETURN QUERY
-	SELECT customer_id, customer_name, reward_points
-	FROM customer
-	WHERE reward_points = (select max(reward_points)
-						   from customer);
-END;
-$$ LANGUAGE plpgsql;
-
-
- -- question 5 
--- Create a view to find out the total sales amount made by each cashier along with their 
--- full name and position for a given time period.
-
-
-CREATE OR REPLACE VIEW sales_by_employee AS
-	SELECT e.employee_id, e.full_name, e.position,
-		   SUM(si.price) AS total_sales_amount
-	FROM transaction t
-	INNER JOIN employee e ON t.cashier_id = e.employee_id
-	INNER JOIN sold_item si ON t.trans_id = si.trans_id
-	WHERE t.trans_date BETWEEN '2023-08-11 00:00:00' AND '2023-09-16 00:00:00'
-	GROUP BY e.employee_id, e.full_name, e.position;
-	
-
-select * from
-sales_by_employee;
-
--- question 6
--- Identify the top 5 customers who have made the most transactions, 
--- along with the count of transactions they have made and the total amount spent.
-CREATE OR REPLACE VIEW top_buyers as
-	SELECT 
-		c.customer_id,
-		c.customer_name,
-		COUNT(t.trans_id) AS transaction_count,
-		SUM(p.total_amount) AS total_amount_spent,
-		dense_rank() over (order by COUNT(t.trans_id) desc)
-	FROM customer c
-	JOIN transaction t ON c.customer_id = t.customer_id
-	JOIN payment p ON t.payment = p.payment_id
-	GROUP BY c.customer_id, c.customer_name;
-	
-select * from top_buyers;
-
-
-
--- question 7
--- Find the average quantity of items sold per transaction for each payment method (cash, credit, debit) 
--- and for each item category.
-
-SELECT 
-    p.cash,
-    p.credit,
-    p.debit,
-    i.category,
-    AVG(si.quantity) AS avg_quantity_sold
-FROM transaction t
-JOIN sold_item si ON t.trans_id = si.trans_id
-JOIN item i ON si.item_id = i.item_id
-JOIN payment p ON t.payment = p.payment_id
-GROUP BY p.cash, p.credit, p.debit, i.category
-order by 4;
-
-/*
-
-#9 - Create a view to create rankings of times of the day (morning, afternoon, night) based on sales in order to have a better idea of when the store is busier. 
-#10 -  together with its category, price, etc. 
-
-*/
-
--- #8 - Update reward points on the customer table based on tot_payment after a new row is inserted in the transaction table. 
--- Creating Function 
-CREATE OR REPLACE FUNCTION update_reward_points()
-RETURNS TRIGGER AS $$
-DECLARE
-    total_payment NUMERIC;
-BEGIN
-    -- Get the total amount from the payment table
-    SELECT total_amount INTO total_payment
-    FROM payment
-    WHERE payment_id = NEW.payment;
-
-    -- $10 spent earns 1 reward point
-    UPDATE customer
-    SET reward_points = reward_points + total_payment / 10
-    WHERE customer_id = NEW.customer_id;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- -- Creating Trigger to update customer reward points
-CREATE TRIGGER trg_after_transaction_insert
-AFTER INSERT ON transaction
-FOR EACH ROW
-EXECUTE FUNCTION update_reward_points();
-
-
--- #9  Create a view to create rankings of times of the day (morning, afternoon, evening)
+-- #3  Create a view to create rankings of times of the day (morning, afternoon, evening)
 -- based on sales in order to have a better idea of when the store is bussier.
 CREATE OR REPLACE VIEW transactions_time_ranking AS
 SELECT
@@ -140,7 +27,7 @@ ORDER BY transaction_rank;
 SELECT * FROM transactions_time_ranking
 
 
--- #10 Create a function that takes a month and year as argument and returns the id of the item that sold the most during that month.
+-- #4 Create a function that takes a month and year as argument and returns the id of the item that sold the most during that month.
 CREATE OR REPLACE FUNCTION most_sold_item_id(month_input INT, year_input INT)
 RETURNS NUMERIC AS
 $$
@@ -164,8 +51,65 @@ $$ LANGUAGE plpgsql;
 SELECT * FROM most_sold_item_id(8, 2023);
 
 
+-- Query 5 - Function to Get Employee Information by Name
+CREATE OR REPLACE FUNCTION get_employee_info(employee_full_name VARCHAR)
+RETURNS TABLE(employee_id NUMERIC, full_name VARCHAR, phone NUMERIC, email VARCHAR, SSN NUMERIC, start_date DATE, salary_hour NUMERIC, "position" VARCHAR) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT employee_id, full_name, phone, email, SSN, start_date_id, salary_hour, position
+    FROM employee
+    WHERE full_name = employee_full_name;
+END;
+$$ LANGUAGE plpgsql;
 
 
+-- Query 6- List off employees' information
+SELECT * FROM employee;
+
+-- Query 7: Employee with Most Sales in a Given Month and Year
+CREATE OR REPLACE FUNCTION get_employee_info(employee_full_name VARCHAR)
+RETURNS TABLE(employee_id NUMERIC, full_name VARCHAR, phone NUMERIC, email VARCHAR, SSN NUMERIC, start_date DATE, salary_hour NUMERIC, "position" VARCHAR) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT employee_id, full_name, phone, email, SSN, start_date_id, salary_hour, position
+    FROM employee
+    WHERE full_name = employee_full_name;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION top_selling_employee(month INTEGER, year INTEGER)
+RETURNS TABLE(employee_id NUMERIC, full_name VARCHAR, total_sales NUMERIC) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT e.employee_id, e.full_name, COUNT(s.trans_id) AS total_sales
+    FROM employee e
+    JOIN transaction t ON e.employee_id = t.cashier_id
+    WHERE EXTRACT(MONTH FROM t.trans_date) = month AND EXTRACT(YEAR FROM t.trans_date) = year
+    GROUP BY e.employee_id, e.full_name
+    ORDER BY total_sales DESC
+    LIMIT 1;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Query 8: Most Valuable Customer Based on Rewards Points
+SELECT customer_id, customer_name, reward_points
+FROM customer
+ORDER BY reward_points DESC
+LIMIT 1;
+
+-- Query 9: List All Customers and Their Information
+SELECT * FROM customer;
+
+-- Query 10: This function checks if a user with the given username and password exists in the database and returns true if so, otherwise false.
+CREATE OR REPLACE FUNCTION login(username VARCHAR, password VARCHAR)
+RETURNS BOOLEAN AS $$
+DECLARE
+    user_found BOOLEAN;
+BEGIN
+    SELECT EXISTS(SELECT 1 FROM admin WHERE username = username AND password = password) INTO user_found;
+    RETURN user_found;
+END;
+$$ LANGUAGE plpgsql;
 	
 
 
